616d2270fa7df3698f32d1cc269959b9bcba61ec
WilsonShaohua <48817671+WilsonShaohua@users.noreply.github.com>
2020-06-04 15:15:36 +0800

    /now /hot /comming 接口实现分页功能、检索功能
    Signed-off-by: WilsonShaohua <48817671+WilsonShaohua@users.noreply.github.com>

--- src/main/java/com/xhu/controller/MovieController.java
+++ src/main/java/com/xhu/controller/MovieController.java
@@ -81,20 +81,20 @@ public class MovieController {
         log.info("request json " + jsonObject.toJSONString());
         Condition condition = jsonObject.getObject("condition", Condition.class);
         List<MoviePo> moviePos = now();
-        Map<String, Object> data = new HashMap<>();
         int code = StateCode.FAIL;
         if (moviePos != null)
             code = StateCode.SUCCESS;
         if (condition != null) {
             moviePos = moviePoService.selectByScreeningConditions(condition, moviePos);
-            data.put("size", moviePos.size());
             moviePos = PageUtils.page(condition.getPageNo(), ConstantString.DEFAULT_MENU_PAGE_SIZE, moviePos);
-        } else {
-            data.put("size", moviePos.size());
         }
 
-        data.put("movie", moviePos);
-        jsonObject = JSONUtils.packageJson(code, StateCode.MSG.get(code), data);
+
+        jsonObject = JSONUtils.packageJson(code, StateCode.MSG.get(code), moviePos);
+        if (moviePos != null)
+            jsonObject.put("size", moviePos.size());
+        else
+            jsonObject.put("size", 0);
         log.info("/movie/now response\n" + jsonObject.toJSONString());
         //修正数据字符集
         response.setContentType("text/html;charset=utf-8");
@@ -128,17 +128,15 @@ public class MovieController {
         int code = StateCode.FAIL;
         if (moviePos != null)
             code = StateCode.SUCCESS;
-        Map<String, Object> data = new HashMap<>();
         if (condition != null) {
             moviePos = moviePoService.selectByScreeningConditions(condition, moviePos);
             moviePos = PageUtils.page(condition.getPageNo(), ConstantString.DEFAULT_MENU_PAGE_SIZE, moviePos);
-            data.put("size", moviePos.size());
-        } else {
-            data.put("size", moviePos.size());
         }
-
-        data.put("movie", moviePos);
-        jsonObject = JSONUtils.packageJson(code, StateCode.MSG.get(code), data);
+        jsonObject = JSONUtils.packageJson(code, StateCode.MSG.get(code), moviePos);
+        if (moviePos != null)
+            jsonObject.put("size", moviePos.size());
+        else
+            jsonObject.put("size", 0);
         log.info("/movie/comming response \n" + jsonObject.toJSONString());
         //修正数据字符集
         response.setContentType("text/html;charset=utf-8");
@@ -180,8 +178,12 @@ public class MovieController {
         int code = StateCode.FAIL;
         if (moviePos != null)
             code = StateCode.SUCCESS;
-        //    moviePos = PageUtils.page(pageNo, ConstantString.DEFAULT_MENU_PAGE_SIZE, moviePos);
+
         JSONObject jsonObject = JSONUtils.packageJson(code, StateCode.MSG.get(code), moviePos);
+        if (moviePos != null)
+            jsonObject.put("size", moviePos.size());
+        else
+            jsonObject.put("size", 0);
         log.info("/movie/hot response: \n" + jsonObject.toJSONString());
         //修正数据字符集
         response.setContentType("text/html;charset=utf-8");
@@ -298,19 +300,17 @@ public class MovieController {
         //获取电影信息
         List<MoviePo> moviePos = top100();
         int code = StateCode.FAIL;
-        Map<String, Object> data = new HashMap<>();
         if (moviePos != null)
             code = StateCode.SUCCESS;
         if (condition != null) {
             moviePos = moviePoService.selectByScreeningConditions(condition, moviePos);
             moviePos = PageUtils.page(condition.getPageNo(), ConstantString.DEFAULT_MENU_PAGE_SIZE, moviePos);
-            data.put("size", moviePos.size());
-        } else {
-            data.put("size", moviePos.size());
         }
-
-        data.put("movie", moviePos);
-        jsonObject = JSONUtils.packageJson(code, StateCode.MSG.get(code), data);
+        jsonObject = JSONUtils.packageJson(code, StateCode.MSG.get(code), moviePos);
+        if (moviePos != null)
+            jsonObject.put("size", moviePos.size());
+        else
+            jsonObject.put("size", 0);
         String jsonString = jsonObject.toJSONString();
         log.info("/movie/top100 response : \n" + jsonString);
         //修正数据字符集
